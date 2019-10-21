036de3d18224ad393a24d2d9cb470ac483b9aae6
NEULET Joachim <n18019101@L-054011F110-18.salsa.univ-amu.fr>
2019-10-21 16:48:30 +0200

    commit iftun.c

--- VMs/partage/iftun.c
+++ VMs/partage/iftun.c
@@ -1,5 +1,21 @@
 #include "iftun.h"
 
 void iftun(int src, int dest){
+    char buffer[SIZE_BUFFER];
+    if(src < 0 || dest < 0){
+        perror("Oops, y'a R");
+        exit(1);
+    }
 
+    int res = read(src, buffer, SIZE_BUFFER);
+
+    if(res < 0){
+        perror("Erreur read");
+    }
+
+    int wr = write(buffer, dest, SIZE_BUFFER);
+
+    if(wr < 0){
+        perror("Erreur write");
+    }
 }
