f7d28215b34f19551f35b2fff5545863ed8115f2
bigby347 <alexandreputzu@gmail.com>
2019-10-30 15:47:16 +0100

    modif test_extremite.c ( test de ext_out)

--- VMs/partage/test_extremite.c
+++ VMs/partage/test_extremite.c
@@ -4,17 +4,18 @@
 int main(int argc, char** argv) {
 
   if (atoi(argv[1]) == 1) {
-    /*int tunfd;
+    int tunfd;
     printf("Création de %s\n",argv[3]);
     tunfd = tun_alloc(argv[3]);
     printf("Faire la configuration de %s...\n",argv[3]);
     printf("Appuyez sur une touche pour continuer\n");
     getchar();
     printf("Interface %s Configurée:\n",argv[1]);
-    system("ip addr"); */
+    system("ip addr");
     char* ip_addr = argv[2];
+    printf("---------------------\n");
     printf("Lancement du serveur\n");
-    ext_out(ip_addr);
+    ext_out(ip_addr,tunfd);
   }
 
   if (atoi(argv[1]) == 2) {
@@ -25,9 +26,10 @@ int main(int argc, char** argv) {
     system("/mnt/partage/config-tun-VM1-6.sh");
     getchar();
     printf("Interface %s Configurée:\n",argv[4]);
-
+    system("ip addr");
     char* ip_addr = argv[2];
     int port = atoi(argv[3]);
+    printf("---------------------\n");
     printf("Lancement du client \n");
     sleep(10);
     ext_int(ip_addr, port,tunfd);
